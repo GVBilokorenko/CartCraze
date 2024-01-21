4089c63f6d8b22b236f02e8e3d4ccc20f4fcf8f4
GVBilokorenko <abilokorenko@gmail.com>
2024-01-21 14:16:33 +0200

    env setup

---
+++ frontend/src/Router.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from 'Layout';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>Home</div>} />
+          <Route path="blogs" element={<div>Blogs</div>} />
+          <Route path="contact" element={<div>Contacts</div>} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
