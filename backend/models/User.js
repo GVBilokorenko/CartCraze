5e7e0b5eb790c52804914398f282bbd9a2d56759
GVBilokorenko <abilokorenko@gmail.com>
2024-01-21 20:45:21 +0200

    handle resresh token (backend)

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,11 @@ import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  phone: { type: String, required: true },
+  refreshToken: String,
   role: { type: String, enum: ['admin', 'user'], default: 'user' }
 });
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,31 +1,63 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 import { protect, restrictTo } from '../middleware/auth.js';
 
 const router = express.Router();
 
-const signToken = id => {
+const signAccessToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
 
+const signRefreshToken = (id) => {
+  return jwt.sign({ id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN });
+};
+
 router.post('/signup', async (req, res) => {
-  const { username, email, password } = req.body;
-  const user = await User.create({ username, email, password });
-  const token = signToken(user._id);
-  res.status(201).json({ token });
+  const { username, email, password, name, surname, phone } = req.body;
+  // const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await User.create({ username, email, password: password, name, surname, phone });
+
+  const accessToken = signAccessToken(user._id);
+  const refreshToken = signRefreshToken(user._id);
+
+  user.refreshToken = refreshToken;
+  await user.save();
+
+  res.status(201).json({ accessToken, refreshToken });
 });
 
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email }).select('+password');
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).select('+password');
 
-  if (!user || !(await user.comparePassword(password))) {
-    return res.status(401).json({ msg: 'Incorrect email or password' });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ msg: 'Incorrect username or password' });
   }
 
-  const token = signToken(user._id);
-  res.status(200).json({ token });
+  const accessToken = signAccessToken(user._id);
+  const refreshToken = signRefreshToken(user._id);
+
+  user.refreshToken = refreshToken;
+  await user.save();
+
+  res.status(200).json({ accessToken, refreshToken });
+});
+
+
+router.post('/token', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) return res.sendStatus(401);
+
+  const user = await User.findOne({ refreshToken }).exec();
+  if (!user) return res.sendStatus(403); // Forbidden if token is not found
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = signAccessToken(user._id);
+    res.json({ accessToken });
+  });
 });
 
 // Example of a protected route
